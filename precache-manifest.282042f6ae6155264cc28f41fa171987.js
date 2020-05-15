4e21bf5a64825a30fae98df952462fdbe0851c99
abdul-54321 <abdulcheemaswl@gmail.com>
2020-05-16 00:05:54 +0500

    Updates

---
+++ precache-manifest.282042f6ae6155264cc28f41fa171987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3871bc7feb718462fd73d2a8cfc0203",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "65df3927fc177dd6a066",
+    "url": "/monstors-rolodex/static/css/main.50e25d07.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstors-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65df3927fc177dd6a066",
+    "url": "/monstors-rolodex/static/js/main.9af5ca5d.chunk.js"
+  },
+  {
+    "revision": "8cfa0c5cf063f7a2c523",
+    "url": "/monstors-rolodex/static/js/runtime-main.5e963daa.js"
+  }
+]);
